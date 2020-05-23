48f32eaefbd0679e23d55417c08ca0771d488c5c
Dani Lyn <dlyn1110@gmail.com>
2020-05-23 19:12:34 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: { message: 'Hashes of data will get converted to JSON' }
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
 end
 end
